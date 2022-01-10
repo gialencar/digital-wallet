89fe7171bae83c7f8f86c09c33ff7ec9fee81ddf
Gilson Alencar <gilson.alencar94@gmail.com>
2022-01-10 19:34:44 -0300

    fix: typo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Switch>
       <Route path="/" exact component={ Login } />
-      <Route path="carteira" component={ Wallet } />
+      <Route path="/carteira" component={ Wallet } />
     </Switch>
   );
 }

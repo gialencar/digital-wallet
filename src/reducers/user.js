aeb569ca4dd5cfafbd57a6ce407f5e35c8af6f74
Gilson Alencar <gilson.alencar94@gmail.com>
2022-01-10 17:44:03 -0300

    feat: setup redux

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,29 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case LOGIN:
+    return {
+      ...state,
+      // user: {
+      //   email: action.payload,
+      // },
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;

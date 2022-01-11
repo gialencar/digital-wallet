acceb0c61c2c27c09f51011e93d265d59cf0aed5
Gilson Alencar <gilson.alencar94@gmail.com>
2022-01-11 15:34:15 -0300

    feat: wallet redux setup

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,7 +9,7 @@ const INITIAL_STATE = {
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case LOGIN:
-    return { email: action.email };
+    return { email: action.payload };
   default:
     return state;
   }
